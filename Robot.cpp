2d070d53792158718f1d854ed9c65814edc35cbb
isiah-lloyd <robotdude101@gmail.com>
2014-10-14 17:10:14 -0400

    Added variables and an empty function for Teleop Driving

--- Robot.cpp
+++ Robot.cpp
@@ -3,11 +3,36 @@
 class Robot: public IterativeRobot
 {
 private:
+//initialize variables
+	//Driverstation variables
+	Joystick *m_leftStick;
+	Joystick *m_rightStick;
+	Joystick *m_gamepad;
 	LiveWindow *lw;
+
+	//Drivetrain motors
+	Jaguar *m_leftFront;
+	Jaguar *m_rightFront;
+	Jaguar *m_leftBack;
+	Jaguar *m_rightBack;
+
+	RobotDrive *m_robotDrive;
+
+
 
 	void RobotInit()
 	{
+		//Define variables
+		m_leftStick = new Joystick(0);
+		m_rightStick = new Joystick(1);
+		m_gamepad = new Joystick (2);
 		lw = LiveWindow::GetInstance();
+
+		m_leftFront = new Jaguar(3);
+		m_rightFront = new Jaguar(2);
+		m_leftBack = new Jaguar(4);
+		m_rightBack = new Jaguar(1);
+		m_robotDrive = new RobotDrive(m_leftFront,m_rightFront,m_leftBack,m_rightBack);
 	}
 
 	void AutonomousInit()
